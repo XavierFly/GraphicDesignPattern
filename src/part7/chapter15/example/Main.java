54330df5739e59ad6c64eaf4858d6fcef39565c4
XavierFly <xavier_fly@163.com>
2019-05-17 10:47:43 +0800

    Facade.

---
+++ src/part7/chapter15/example/Main.java
@@ -0,0 +1,9 @@
+package part7.chapter15.example;
+
+import part7.chapter15.example.pagemaker.PageMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        PageMaker.makeWelcomePage("hyuku@hyuki.com", "Welcome.html");
+    }
+}
---
+++ src/part7/chapter15/exercise/Main.java
@@ -0,0 +1,9 @@
+package part7.chapter15.exercise;
+
+import part7.chapter15.exercise.pagemaker.PageMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        PageMaker.makeWelcomePage("hyuku@hyuki.com", "Welcome.html");
+    }
+}
